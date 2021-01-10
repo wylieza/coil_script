f6285afaaa061e4a54992ab838c5dd811c09369d
Justin Wylie <wylieza@gmail.com>
2021-01-10 15:20:46 +0200

    Code to generate script implemented

--- Coil.java
+++ Coil.java
@@ -1,15 +1,17 @@
 import java.util.Scanner;
+import java.io.FileWriter;
+import java.io.IOException;
 
 /*
 Author: Justin Wylie
 
 Description:
-    A basic program to produce a PCB coil. Outputs an eagle compatible script.
+    A basic program to produce a script which in turn produces a PCB based coil. Outputs an eagle compatible script.
 
 */
 
 public class Coil{
-    static double trace_clearnace = 0.3, track_width = 0.3, width = 54, height = 64;
+    static double trace_clearnace = 0.3, track_width = 0.3, width = 54, height = 64, inner_width = 10;
 
     public static String dimensions_toString(){
         String msg = "";
@@ -80,13 +82,62 @@ public class Coil{
 
     }
 
-    public static void new_corners(double[][] corners){
+    public static double inner_width(double[][] corners){
+        double length_a = corners[1][0] - corners[0][0];
+        double length_b = corners[3][1] - corners[0][1];
+
+        return length_a > length_b ? length_b:length_a; //Return shortest length
+    }
+
+    public static void update_corners(double[][] corners){
+
+        //Bottom Left
+        corners[0][0] = corners[4][0];
+        corners[0][1] = corners[4][1];
+
+        //Next Bottom Left
+        corners[4][0] += trace_clearnace + track_width*0.5;
+        corners[4][1] += trace_clearnace + track_width*0.5;
+
+        //Bottom Right
+        corners[1][0] -= trace_clearnace + track_width*0.5;
+        corners[1][1] += trace_clearnace + track_width*0.5;
+
+        //Top Right
+        corners[2][0] -= trace_clearnace + track_width*0.5;
+        corners[2][1] -= trace_clearnace + track_width*0.5;
+
+        //Top Left
+        corners[3][0] += trace_clearnace + track_width*0.5;
+        corners[3][1] -= trace_clearnace + track_width*0.5;
 
     }
 
     public static void generate_script(){
         //Generate coil here
-        double[][] corners = new double[4][2];
+        double[][] corners = new double[5][2];
+
+        try{
+            init_corners(corners);
+            FileWriter outFile = new FileWriter("script.txt");
+
+            outFile.write("GRID MM;\n");
+            outFile.write("LAYER 1;\n");
+
+            while (inner_width(corners) > inner_width){
+                for (int i = 0; i < 4; i++){
+                    outFile.write("wire " + track_width + " (" + corners[i][0] + " " + corners[i][1] + ") (" + corners[i+1][0] + " " + corners[i+1][1] + ")\n");
+                }
+
+                update_corners(corners);
+            }
+            
+            outFile.close();
+
+        }catch (IOException e){
+            System.out.println("Error while writing to script file");
+            e.printStackTrace();
+        }
 
     }
 
@@ -98,7 +149,7 @@ public class Coil{
             System.out.println("Using default configuration");
         }
 
-
+        generate_script();
 
     }
 
