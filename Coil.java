733c466ae50585ce2da667e4f4af1b577d97a4ea
Justin Wylie <wylieza@gmail.com>
2021-01-10 09:35:37 +0200

    created a ignore file for class files

---
+++ Coil.java
@@ -0,0 +1,18 @@
+
+/*
+Author: Justin Wylie
+
+Description:
+    A basic program to produce a PCB coil. Outputs an eagle compatible script.
+
+*/
+
+public class Coil{
+
+    public static void main(String[] args){
+        
+
+    }
+
+
+}
